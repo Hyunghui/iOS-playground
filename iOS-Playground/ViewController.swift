2896c763ee83aa9aafae24667e77035dee66514e
Peter <hyunghui@beokay.com>
2018-04-25 16:26:53 +0900

    Add views

--- iOS-Playground/ViewController.swift
+++ iOS-Playground/ViewController.swift
@@ -22,9 +22,4 @@ class ViewController: UIViewController {
         DDLogError("Error")
         print("really")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
